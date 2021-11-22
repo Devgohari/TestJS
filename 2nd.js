14b665ee53ccebb8d3a2afe787dc8788bf81496a
Devgohari <devgohari786@outlook.com>
2021-11-23 02:31:21 +0530

    callback and let VAr

--- 2nd.js
+++ 2nd.js
@@ -1,1 +1,13 @@
-aaaa
+// function trial() {
+//     let count = 0
+//     document.getElementById('trial').addEventListener('click', ()=>{
+//         console.log('clicked' , ++count);
+//     })
+// }
+// trial()
+let a = 10;
+{
+let  a = 55;
+   console.log(a);
+}
+console.log(a);
---
+++ callBacks.js
@@ -0,0 +1,15 @@
+console.log('Callbacks');
+setTimeout(() => {
+    console.log("yoyoyoy");
+}, 5000);
+function x(y) {
+    console.log('funcXXXXXXX');
+    y();
+}
+x(function y() {
+    console.log("funcYYYYYYYYYY");
+})
+function trial() {
+    let count = 0
+    
+}
