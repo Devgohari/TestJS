7c0af62d4c5a7fc975374bee1594c39763687e54
Devgohari <devgohari786@outlook.com>
2021-11-23 01:00:40 +0530

    var lett

---
+++ varLetconst.js
@@ -0,0 +1,15 @@
+console.log(b);
+// console.log(a);
+// console.log(c);
+let a = 10
+var b = 100
+const c = 50
+console.log(c);
+// window.b
+
+let namee = 'Dev'
+console.log(namee);
+{let namee = 'NewDev'
+console.log(namee);}
+
+console.log(namee);
